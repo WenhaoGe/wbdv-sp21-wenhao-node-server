8a1ab2a78b58f8d848d59f89dca8296af44d104d
Wenhao Ge <ge.wen@northeastern.edu>
2021-04-11 19:22:14 -0700

    made changes to question-controller

--- controllers/question-controller.js
+++ controllers/question-controller.js
@@ -13,11 +13,9 @@ module.exports = (app) => {
     const findQuestionsForQuiz = (req, res) => {
         const quizId = req.params['qid']
         const questions = questionService.findQuestionsForQuiz(quizId)
-        res.json(questions)
+        res.send(questions)
     }
     app.get("/api/quizzes/:qid/questions", findQuestionsForQuiz);
     app.get("/api/questions", findAllQuestions);
 }
-
-
 
